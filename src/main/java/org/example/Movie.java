3f3ef6dbeefdd29e1339fa81dd5a5dbb6e8e06bc
MKHARBECH <salah_ita@yahoo.fr>
2025-05-03 15:40:16 +0200

    version avec la notation @Data

--- src/main/java/org/example/Movie.java
+++ src/main/java/org/example/Movie.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.net.URL;
 
 /*
@@ -15,7 +20,7 @@ import java.net.URL;
 @EqualsAndHashCode(exclude = "minutes")
 */
 // Annotation qui remplace toutes les précedentes.
-
+@Data
 public class Movie {
     private Long id;
     private String title;
@@ -25,62 +30,10 @@ public class Movie {
     private String country;
     private String language;
     // ajouter une annotaion pour le niveau d'acces à l'attribut
-    // @Getter(AccessLevel.PRIVATE)
-    //private final int minutes = 120;
+    @Getter(AccessLevel.PRIVATE)
+    private final int minutes = 120;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
-    public String getTitle() {
-        return title;
-    }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
-
-    public URL getOfficialSite() {
-        return officialSite;
-    }
-
-    public void setOfficialSite(URL officialSite) {
-        this.officialSite = officialSite;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
 }
