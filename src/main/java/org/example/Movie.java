177ca7efb2de6f7c2f8a57fe6dea5d353a6af12e
MKHARBECH <salah_ita@yahoo.fr>
2025-05-03 14:52:17 +0200

    version avant lombok avec les getter et les setter

--- src/main/java/org/example/Movie.java
+++ src/main/java/org/example/Movie.java
@@ -2,14 +2,31 @@ package org.example;
 
 import java.net.URL;
 
+/*
+// Générer un getter et un setter
+@Getter @Setter
+// Générer un constructeur sans arguments
+@NoArgsConstructor
+// Générer un constructeur avec tous les arguments
+@AllArgsConstructor
+//Génerer la méthode tiString sans l'attribut "minutes".
+@ToString(exclude = "minutes")
+//Génerer la méthode Equals() et HashCode().
+@EqualsAndHashCode(exclude = "minutes")
+*/
+// Annotation qui remplace toutes les précedentes.
+
 public class Movie {
     private Long id;
     private String title;
-    private String descreption;
+    private String description;
     private int rating;
     private URL officialSite;
     private String country;
     private String language;
+    // ajouter une annotaion pour le niveau d'acces à l'attribut
+    // @Getter(AccessLevel.PRIVATE)
+    //private final int minutes = 120;
 
     public Long getId() {
         return id;
@@ -27,12 +44,12 @@ public class Movie {
         this.title = title;
     }
 
-    public String getDescreption() {
-        return descreption;
+    public String getDescription() {
+        return description;
     }
 
-    public void setDescreption(String descreption) {
-        this.descreption = descreption;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public int getRating() {
--- src/test/java/org/example/MovieTest.java
+++ src/test/java/org/example/MovieTest.java
@@ -1,64 +1,113 @@
 package org.example;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MovieTest {
-
-    @Test
+    private Movie movie;
+    @BeforeEach
+    void setUp() throws MalformedURLException {
+        movie = new Movie();
+        movie.setTitle("Harry Potter");
+        movie.setDescription("The Harry Potter");
+        movie.setCountry("United States");
+        movie.setLanguage("English");
+        movie.setRating(8);
+        movie.setOfficialSite(new URL("https://www.moviesofamerica.com"));
+        movie.setId(1L);
+    }
+    @org.junit.jupiter.api.Test
     void getId() {
+        assertNotNull(movie);
+        assertEquals(1L, movie.getId());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void setId() {
+        assertNotNull(movie);
+        movie.setId(2L);
+        assertEquals(2L, movie.getId());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void getTitle() {
+        assertNotNull(movie);
+        assertEquals("Harry Potter", movie.getTitle());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void setTitle() {
+        assertNotNull(movie);
+        movie.setTitle("Harry Potter2");
+        assertEquals("Harry Potter2", movie.getTitle());
     }
 
-    @Test
-    void getDescreption() {
+    @org.junit.jupiter.api.Test
+    void getDescription() {
+        assertNotNull(movie);
+        assertEquals("The Harry Potter", movie.getDescription());
     }
 
-    @Test
-    void setDescreption() {
+    @org.junit.jupiter.api.Test
+    void setDescription() {
+        assertNotNull(movie);
+        movie.setDescription("The Harry Potter2");
+        assertEquals("The Harry Potter2", movie.getDescription());
     }
 
-    @Test
-    void getRating() {
-    }
-
-    @Test
-    void setRating() {
-    }
-
-    @Test
-    void getOfficialSite() {
-    }
-
-    @Test
-    void setOfficialSite() {
-    }
-
-    @Test
+    @org.junit.jupiter.api.Test
     void getCountry() {
+        assertNotNull(movie);
+        assertEquals("United States", movie.getCountry());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void setCountry() {
+        assertNotNull(movie);
+        movie.setCountry("France");
+        assertEquals("France", movie.getCountry());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
+    void getRating() {
+        assertNotNull(movie);
+        assertEquals(8, movie.getRating());
+    }
+
+    @org.junit.jupiter.api.Test
+    void setRating() {
+        assertNotNull(movie);
+        movie.setRating(10);
+        assertEquals(10, movie.getRating());
+    }
+
+    @org.junit.jupiter.api.Test
+    void getOfficialSite() {
+        assertNotNull(movie);
+        assertEquals("https://www.moviesofamerica.com", movie.getOfficialSite().toString());
+    }
+
+    @org.junit.jupiter.api.Test
+    void setOfficialSite() throws MalformedURLException {
+        assertNotNull(movie);
+        movie.setOfficialSite(new URL("https://www.moviesofamerica.fr"));
+        assertEquals("https://www.moviesofamerica.fr", movie.getOfficialSite().toString());
+    }
+
+    @org.junit.jupiter.api.Test
     void getLanguage() {
+        assertNotNull(movie);
+        assertEquals("English", movie.getLanguage());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     void setLanguage() {
+        assertNotNull(movie);
+        movie.setLanguage("french");
+        assertEquals("french", movie.getLanguage());
     }
 }
